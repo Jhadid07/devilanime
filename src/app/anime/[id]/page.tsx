e127ff8c1b01fcf9b6ac249d98b9a3cf95c9028b
Abdul Jadid <ajtokkong@gmail.com>
2026-05-16 09:07:56 +0800

    added anime recommendations

--- src/app/anime/[id]/page.tsx
+++ src/app/anime/[id]/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 type Props = {
   params: Promise<{
     id: string;
@@ -11,10 +13,21 @@ async function getAnime(id: string) {
   return data.data;
 }
 
+async function getRecommendations(id: string) {
+  const res = await fetch(
+    `https://api.jikan.moe/v4/anime/${id}/recommendations`
+  );
+
+  const data = await res.json();
+
+  return data.data;
+}
+
 export default async function AnimePage({ params }: Props) {
   const { id } = await params;
 
   const anime = await getAnime(id);
+  const recommendations = await getRecommendations(id);
 
   return (
     <main className="min-h-screen bg-black text-white p-10">
@@ -35,19 +48,33 @@ export default async function AnimePage({ params }: Props) {
             {anime.synopsis}
           </p>
 
-          <div className="mt-6 space-y-3 text-lg">
+          <div className="mt-6 space-y-4 text-lg">
 
-            <p>⭐ Score: {anime.score}</p>
+  <p>⭐ Score: {anime.score}</p>
 
-            <p>🎬 Episodes: {anime.episodes}</p>
+  <p>🎬 Episodes: {anime.episodes}</p>
 
-            <p>📺 Status: {anime.status}</p>
+  <p>📺 Status: {anime.status}</p>
 
-            <p>🏆 Rank: #{anime.rank}</p>
+  <p>🏆 Rank: #{anime.rank}</p>
 
-            <p>❤️ Favorites: {anime.favorites}</p>
+  <p>❤️ Favorites: {anime.favorites}</p>
 
-          </div>
+  <p>📅 Year: {anime.year}</p>
+
+  <p>
+    🎨 Genres:
+    {" "}
+    {anime.genres.map((genre: any) => genre.name).join(", ")}
+  </p>
+
+  <p>
+    🏢 Studio:
+    {" "}
+    {anime.studios.map((studio: any) => studio.name).join(", ")}
+  </p>
+
+</div>
 
           {anime.trailer?.embed_url && (
   <div className="mt-10">
@@ -70,6 +97,45 @@ export default async function AnimePage({ params }: Props) {
         </div>
 
       </div>
+
+      <div className="max-w-6xl mx-auto mt-20">
+
+  <h2 className="text-4xl font-bold text-purple-500 mb-8">
+    Recommended Anime
+  </h2>
+
+  <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+    {recommendations.slice(0, 6).map((rec: any, index: number) => (
+
+      <Link
+        key={`${rec.entry.mal_id}-${index}`}
+        href={`/anime/${rec.entry.mal_id}`}
+        className="bg-gray-900 rounded-2xl overflow-hidden hover:scale-105 transition"
+      >
+
+        <img
+          src={rec.entry.images.jpg.image_url}
+          alt={rec.entry.title}
+          className="w-full h-72 object-cover"
+        />
+
+        <div className="p-4">
+
+          <h3 className="font-bold text-lg">
+            {rec.entry.title}
+          </h3>
+
+        </div>
+
+      </Link>
+
+    ))}
+
+  </div>
+
+</div>
+
     </main>
   );
 }
